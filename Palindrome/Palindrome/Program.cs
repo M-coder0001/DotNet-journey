228a35935ed5ad83e87fc42cb30c40dfdf9646ad
M-coder0001 <mayankchandrapal@gmail.com>
2026-06-22 10:28:01 +0530

    Palindrome program

---
+++ Palindrome/Palindrome/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string str ,rev = "";
+            Console.Write("Enter a string: ");
+            str = Console.ReadLine();
+            int i, l;
+            l = str.Length - 1;
+
+            for (i = l; i >= 0; i--)
+            {
+                rev = rev + str[i];
+            }
+            if (rev == str)
+            {
+                Console.WriteLine("The string is a palindrome.");
+            }
+            else
+            {
+                Console.WriteLine("The string isn't a palindrome.");
+            }
+        }
+
+    }
+}
