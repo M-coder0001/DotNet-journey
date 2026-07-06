540bb5b81efa822ded979a4c8c8c4154a2ffcb6e
M-coder0001 <mayankchandrapal@gmail.com>
2026-07-06 18:11:14 +0530

    New program added

---
+++ OneDArray/OneDArray/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OneDArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int[] arr = {100, 200, 300, 400, 500 };
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine($"Recent Trandaction is {i}: {arr[i]}");
+            }
+        }
+    }
+}
