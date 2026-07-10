0df4fa2da777a1593dd74535d01ed12674fdc2f0
M-coder0001 <mayankchandrapal@gmail.com>
2026-07-10 08:56:40 +0530

    New program added

---
+++ Constructor_and_Distructor/Constructor_and_Distructor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+public class Constructor_and_Destructor
+{
+        public Constructor_and_Destructor()
+        {
+            Console.WriteLine("Resource Initialized (Constructor Invoked)");
+        }
+        ~Constructor_and_Destructor()
+        {
+            Console.WriteLine("Resource Cleaned Up (Destructor Invoked)");
+            Console.ReadKey();
+        }
+}
+class TestResources
+{   
+    public static void Main(string[] args)
+    {
+        Constructor_and_Destructor rm = new Constructor_and_Destructor();
+    }
+}
