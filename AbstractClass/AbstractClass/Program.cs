c6899c130d0d391a248f5ea0c868a9a24570abed
M-coder0001 <mayankchandrapal@gmail.com>
2026-07-15 11:07:37 +0530

    New program added

--- AbstractClass/AbstractClass/Program.cs
+++ AbstractClass/AbstractClass/Program.cs
@@ -1,1 +1,33 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+public abstract class Vehicle
+{
+    public abstract void StartEngine();
+}
+public class Car : Vehicle
+{
+    public override void StartEngine()
+    {
+        Console.WriteLine("Car engine started");
+    }
+}
+public class Motorcycle : Vehicle
+{
+    public override void StartEngine()
+    {
+        Console.WriteLine("Motorcycle engine started..");
+    }
+}
+public class TestAbstract
+{
+    public static void Main()
+    {
+        Vehicle myVehicle;
+
+        myVehicle = new Car();
+        myVehicle.StartEngine();
+
+        myVehicle = new Motorcycle();
+        myVehicle.StartEngine();
+    }
+}
