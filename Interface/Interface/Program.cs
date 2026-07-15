cdd0f2d4833f01adfdc99075ec1489786a7b2a37
M-coder0001 <mayankchandrapal@gmail.com>
2026-07-15 11:39:45 +0530

    New program added

---
+++ Interface/Interface/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public interface IOrderService
+{
+    void ProcessOrder();
+}
+public class StandardDelivery : IOrderService
+{
+    public void ProcessOrder() 
+    {
+        Console.Write("Enter item name for standard Delivery: ");
+        string item = Console.ReadLine();
+        Console.WriteLine("Order placed! For the Product " + item + " and Express delivery will arrive TODAY!");
+    }
+}
+public class ExpressDelivery : IOrderService
+{
+    public void ProcessOrder()
+    {
+        Console.Write("Enter item name for Express Delivery: ");
+        string item = Console.ReadLine();
+        Console.WriteLine("Order placed! For the Product " + item + " and Express delivery will arrive TODAY!");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        IOrderService service = new StandardDelivery();
+        service.ProcessOrder();
+
+        ExpressDelivery service1 = new ExpressDelivery();
+        service.ProcessOrder();
+        
+        Console.ReadKey();
+    }
+}
