1ad5299f43d3d65944d047dd89c83bb3e72b60e2
M-coder0001 <mayankchandrapal@gmail.com>
2026-07-20 11:39:37 +0530

    New program added

---
+++ Multilevel_Multiple3/Multilevel_Multiple3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+public class Person
+{
+    public string name = "Mayank";
+}
+public class Student : Person
+{
+    public int RollNo = 502;
+}
+public class ResearchStudent : Student
+{
+    public string ResearchTopic = "Quantum Computing";
+}
+public interface SportsActivity
+{
+    void PlayMatch();
+}
+public interface CulturalActivity
+{
+    void ParticipateInEvent();
+}
+public class AllRounderResearchStudent : ResearchStudent, SportsActivity, CulturalActivity
+{   public void PlayMatch()
+    {
+        Console.WriteLine("Sports: Represented the iniversity in the Inter-College Chess Match");
+    }
+    public void ParticipateInEvent()
+    {
+        Console.WriteLine("Cultural: Participated as a lead guitarist in the Annual Fest");
+    }
+}
+class Multilevel_Multiple3
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("===Multilevel Interface===");
+        AllRounderResearchStudent student = new AllRounderResearchStudent();
+        Console.WriteLine("Student Name: " + student.name);
+        Console.WriteLine("Rol Np: " + student.RollNo);
+        Console.WriteLine("Research Topic: " + student.ResearchTopic);
+
+        Console.WriteLine();
+
+        Console.WriteLine("===Multiple Interface (Class + Interface)===");
+        student.PlayMatch();
+        student.ParticipateInEvent();
+    }
+}
