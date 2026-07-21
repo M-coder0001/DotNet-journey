8314f05f26b8951fc6f660453a8e544595df1919
M-coder0001 <mayankchandrapal@gmail.com>
2026-07-21 17:15:50 +0530

    New program added

---
+++ MultipleCatchBlocks/MultipleCatchBlocks/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.Write("Enter a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            object str = "Hello";
+            int value = (int)str;
+            Console.WriteLine("Value = " + value);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Error: Invalid Number format");
+        }
+        catch (InvalidCastException)
+        {
+            Console.WriteLine("Error: Invalid type casting");
+        }
+        finally
+        {
+            Console.WriteLine("Example of multiple catch blocks");
+        }
+    }
+}
