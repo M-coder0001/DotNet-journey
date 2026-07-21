57a89a8752c4f8136291f7971d7763e8e235b84b
M-coder0001 <mayankchandrapal@gmail.com>
2026-07-21 18:11:34 +0530

    New program added

---
+++ UserDefineExceptions/UserDefineExceptions/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace UserDefineExceptions
+{
+    public class InvalidAgeException : Exception
+    {
+        public InvalidAgeException(string msg) : base (msg) 
+        {
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                try
+                {
+                    Console.Write("Enter age: ");
+                    int age = Convert.ToInt16(Console.ReadLine());
+
+                    if (age < 18)
+                    {
+                        throw new InvalidAgeException("Sorry, Age must be greater than 18");
+                    }
+                }
+                catch (InvalidAgeException e)
+                {
+                    Console.WriteLine (e);
+                }
+                Console.WriteLine("You are eligible for vote");
+            }
+        }
+    }
+}
