1f2bf3de3df1be53963c65e985c068c7081a94c6
M-coder0001 <mayankchandrapal@gmail.com>
2026-08-03 21:27:31 +0530

    new programs added

---
+++ Properties2/Properties2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Properties2
+{
+    class BankAccount
+    {
+        private double balance;
+
+        public double Balance
+        {
+            get 
+            { 
+                return balance; 
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    balance = value;
+                }
+                else
+                {
+                    Console.WriteLine("Balance cannot be negative.");
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount();
+            account.Balance = 0; // Set the balance to a positive value
+            Console.WriteLine($"Balance: {account.Balance}");
+        }
+    }
+}
