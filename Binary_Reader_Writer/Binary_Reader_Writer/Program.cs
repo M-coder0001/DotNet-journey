ca01738d4a36e79b8eedea497c84bc2c989c69e2
M-coder0001 <mayankchandrapal@gmail.com>
2026-08-07 07:58:06 +0530

    new programs added

---
+++ Binary_Reader_Writer/Binary_Reader_Writer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace Binary_Reader_Writer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (BinaryWriter writer = new BinaryWriter(File.Open("D:\\BinaryFile.dat", FileMode.Create)))
+            {
+                writer.Write(42);
+                writer.Write(3.14);
+                writer.Write("Hello, World!");
+            }
+            using (BinaryReader reader = new BinaryReader(File.Open("D:\\BinaryFile.dat", FileMode.Open)))
+            {
+                int intValue = reader.ReadInt32();
+                double doubleValue = reader.ReadDouble();
+                string stringValue = reader.ReadString();
+                Console.WriteLine($"Read values: {intValue}, {doubleValue}, {stringValue}");
+            }
+        }
+    }
+}
